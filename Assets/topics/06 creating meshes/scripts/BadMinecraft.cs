3f6fb82fa8b1b20294368808574c82f2e0713e55
Bowen <1758364810@qq.com>
2024-10-16 18:37:16 -0400

    loaded in class scripts

--- Assets/topics/06 creating meshes/scripts/BadMinecraft.cs
+++ Assets/topics/06 creating meshes/scripts/BadMinecraft.cs
@@ -13,109 +13,95 @@ public class BadMinecraft : MonoBehaviour
         MakeCube();
     }
 
-    void MakeCube()
-    {
-        Vector3[] c =
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(1, 0, 0),
-            new Vector3(1, 1, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(0, 1, 1),
-            new Vector3(1, 1, 1),
-            new Vector3(1, 0, 1),
-            new Vector3(0, 0, 1)
-
-        };
-
-        Vector3[] vertices =
-        {
-            // 0 1 2 3
-            c[0], c[1], c[2], c[3],
-            // 4 5 6 7
-            c[3], c[2], c[5], c[4],
-            // 8 9 10 12
-            c[1], c[6], c[5], c[2],
-            // 12 13 14 15
-            c[0], c[3], c[4], c[7],
-            // 16 17 18 19
-            c[7], c[4], c[5], c[6],
-            // 20 21 22 23
-            c[0], c[1], c[6], c[7]
-
+    void MakeCube() {
+        // define the coordinates of each corner of the cube
+        Vector3[] c = {
+            new Vector3(0, 0, 0), // 0 
+            new Vector3(1, 0, 0), // 1
+            new Vector3(1, 1, 0), // 2
+            new Vector3(0, 1, 0), // 3
+            new Vector3(0, 1, 1), // 4
+            new Vector3(1, 1, 1), // 5
+            new Vector3(1, 0, 1), // 6
+            new Vector3(0, 0, 1)  // 7
         };
         
+        // define the vertices of the cube
+        Vector3[] vertices = {
+        //  0     1     2     3  
+            c[0], c[1], c[2], c[3], // south face
+        
+        //  4     5     6     7
+            c[3], c[2], c[5], c[4], // up face
+            
+        //  8     9     10    11
+            c[1], c[6], c[5], c[2], // east face
+            
+        //  12    13    14    15
+            c[0], c[3], c[4], c[7], // west face
+            
+        //  16    17    18    19
+            c[7], c[4], c[5], c[6], // north face
+            
+        //  20    21    22    23
+            c[0], c[1], c[6], c[7] // down face
+        };
+
+        Vector3 south = Vector3.back;
         Vector3 up = Vector3.up;
+        Vector3 east = Vector3.right;
+        Vector3 west = Vector3.left;
+        Vector3 north = Vector3.forward;
         Vector3 down = Vector3.down;
-        Vector3 front = Vector3.forward;
-        Vector3 back = Vector3.back;
-        Vector3 left = Vector3.left;
-        Vector3 right = Vector3.right;
-        
-        Vector3[] normals =
-        {
-            // 0 1 2 3
-            down, down, down, down,
-            // 4 5 6 7
-            front, front, front, front,
-            // 8 9 10 11
-            right, right, right, right,
-            // 12 13 14 15
-            back, back, back, back,
-            // 16 17 18 19
-            left, left, left, left,
-            // 20 21 22 23
-            up, up, up, up
+
+        Vector3[] normals = {
+            south, south, south, south,
+            up, up, up, up,
+            east, east, east, east,
+            west, west, west, west,
+            north, north, north, north,
+            down, down, down, down
         };
-        
-Vector2[] uv =
-        {// 前面
-            new(0.0f, 0.0f), new(0.5f, 0.0f), new(0.0f, 0.5f), new(0.5f, 0.5f),
 
-// 后面
-            new(0.0f, 0.5f), new(0.5f, 0.5f), new(0.0f, 1.0f), new(0.5f, 1.0f),
-
-// 左面
-            new(0.5f, 0.0f), new(1.0f, 0.0f), new(0.5f, 0.5f), new(1.0f, 0.5f),
-
-// 右面
-            new(0.0f, 0.5f), new(0.5f, 0.5f), new(0.0f, 1.0f), new(0.5f, 1.0f),
-
-// 上面
-            new(0.0f, 0.0f), new(0.5f, 0.0f), new(0.0f, 0.5f), new(0.5f, 0.5f),
-
-// 下面
-            new(0.5f, 0.0f), new(1.0f, 0.0f), new(0.5f, 0.5f), new(1.0f, 0.5f)
+        Vector2[] uvs = {
+            // south
+            new (0.0f, 0.0f), new (0.5f, 0.0f), new (0.5f, 0.5f), new (0.0f, 0.5f),
+            
+            // up 
+            new (0.0f, 0.5f), new (0.5f, 0.5f), new (0.5f, 1.0f), new (0.0f, 1.0f),
+            
+            // east
+            new (0.0f, 0.0f), new (0.5f, 0.0f), new (0.5f, 0.5f), new (0.0f, 0.5f),
+            
+            // west
+            new (0.5f, 0.0f), new (0.5f, 0.5f), new (0.0f, 0.5f), new (0.0f, 0.0f),
+            
+            // north
+            new (0.5f, 0.0f), new (0.5f, 0.5f), new (0.0f, 0.5f), new (0.0f, 0.0f),
+            
+            // down
+            new (0.5f, 0.5f), new (1.0f, 0.5f), new (1.0f, 0.0f), new (0.5f, 0.0f)
         };
-        
-        int[] triangles = 
-        {
-            // 前面
-            0, 2, 1,   0, 3, 2,
 
-            // 后面
-            4, 6, 5,   4, 7, 6,
-
-            // 左面
-            8, 10, 9,  8, 11, 10,
-
-            // 右面
-            12, 14, 13, 12, 15, 14,
-
-            // 上面
-            16, 18, 17, 16, 19, 18,
-
-            // 下面
-            20, 22, 21, 20, 23, 22
+        int[] triangles = {
+            0, 3, 2,    0, 2, 1,    // south
+            4, 7, 6,    4, 6, 5,    // up
+            8, 11, 10,  8, 10, 9,   // east
+            12, 15, 14, 12, 14, 13, // west
+            16, 19, 18, 16, 18, 17, // north
+            20, 21, 22, 20, 22, 23  // down  
         };
 
         mesh = GetComponent<MeshFilter>().mesh;
         mesh.Clear();
         
         mesh.vertices = vertices;
-        mesh.uv = uv;
-        mesh.triangles = triangles;
+        mesh.uv = uvs;
         mesh.normals = normals;
+        mesh.triangles = triangles;
+    }
 
+    void OnDestroy() {
+        Destroy(mesh);
     }
 }
--- Assets/topics/06 creating meshes/scripts/ProceduralCube.cs
+++ Assets/topics/06 creating meshes/scripts/ProceduralCube.cs
@@ -14,6 +14,40 @@ public class ProceduralCube : MonoBehaviour
     }
 
     void MakeCube() {
+        Vector3[] vertices = {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(1, 0, 1),
+            new Vector3(0, 0, 1)
+        };
 
+        int[] triangles = {
+            0, 2, 1, // south face
+            0, 3, 2,
+            3, 4, 5, // top face
+            3, 5, 2,
+            1, 5, 6, // east face
+            1, 2, 5,
+            0, 7, 4, // west face
+            0, 4, 3,
+            7, 5, 4, // north face
+            7, 6, 5,
+            0, 6, 7, // down face
+            0, 1, 6
+        };
+
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+
+
+    void OnDestroy() {
+        Destroy(mesh);
     }
 }
